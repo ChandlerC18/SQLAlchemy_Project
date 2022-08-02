67900a1dafdda01decf5b21a71e62134c6524db8
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-02 15:20:00 -0400

    add creating session and adding objects example

--- sqlalchemy_orm_example.py
+++ sqlalchemy_orm_example.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+# declaring mapping
 # SQL expression:
 # CREATE TABLE customers (
 #    id INTEGER NOT NULL,
@@ -20,3 +22,19 @@ class Customers(Base):
 
 engine = create_engine('sqlite:///sales.db', echo = True)
 Base.metadata.create_all(engine)
+
+# creating session
+Session = sessionmaker(bind = engine)
+session = Session()
+
+# adding objects
+c1 = Customers(name = 'Ravi Kumar', address = 'Station Road Nanded', email = 'ravi@gmail.com') # single object
+session.add(c1)
+session.commit()
+
+session.add_all([ # multiple objects at the same time
+   Customers(name = 'Komal Pande', address = 'Koti, Hyderabad', email = 'komal@gmail.com'),
+   Customers(name = 'Rajender Nath', address = 'Sector 40, Gurgaon', email = 'nath@gmail.com'),
+   Customers(name = 'S.M.Krishna', address = 'Budhwar Peth, Pune', email = 'smk@gmail.com')]
+)
+session.commit()
