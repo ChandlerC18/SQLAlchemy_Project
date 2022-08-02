f713b0bb094c5e7f7512454a29e605f7679a9b18
ChandlerC18 <chandlercheung@Chandlers-MacBook-Pro.local>
2022-08-02 14:15:36 -0400

    add text function example

--- sqlalchemy_example.py
+++ sqlalchemy_example.py
@@ -44,3 +44,22 @@ for row in result: # print each row of table
 # from sqlalchemy.sql import select
 # s = select([users])
 # result = conn.execute(s)
+
+# using the text construct for passing SQL expressions as strings
+from sqlalchemy import text
+t = text("SELECT * FROM students")
+result = connection.execute(t)
+
+s = text("select students.name, students.lastname from students where students.name between :x and :y") # using bound parameters
+conn.execute(s, x = 'A', y = 'L').fetchall()
+
+stmt = text("SELECT * FROM students WHERE students.name BETWEEN :x AND :y")
+stmt = stmt.bindparams( # pre-established bound values
+   bindparam("x", type_= String),
+   bindparam("y", type_= String)
+)
+result = conn.execute(stmt, {"x": "A", "y": "L"})
+
+# from sqlalchemy.sql import select
+# s = select([text("students.name, students.lastname from students")]).where(text("students.name between :x and :y")) # can use the select function
+# conn.execute(s, x = 'A', y = 'L').fetchall()
